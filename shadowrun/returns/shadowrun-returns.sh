718b635a6a6b99efe057c37c330ec56687bb6e4b
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2025-04-08 11:41:24 -0700

    complete PKGBUILD for Shadowrun Returns

--- shadowrun/returns/shadowrun-returns.sh
+++ shadowrun/returns/shadowrun-returns.sh
@@ -3,8 +3,7 @@
 # Shadowrun Returns Launcher
 #
 
-#XPATH=/opt/shadowrun/returns
-XPATH="/home/hildigerr/Games/GOG/Shadowrun Returns/game"
+XPATH=/opt/shadowrun/returns
 LAUNCH=./Shadowrun
 
 argv=()
@@ -12,12 +11,13 @@ while [ "$#" -gt 0 ]; do
   case $1 in
     --editor|-e)
       if [ -e "$XPATH/ShadowrunEditor" ]; then
-        LAUNCH="env LD_LIBRARY_PATH=. ./ShadowrunEditor"
+        export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/Shadowrun"
+        LAUNCH="xdg-launch ./ShadowrunEditor --"
       else
         echo "Error: The Shadowrun Returns Editor is not installed."
         exit 1
       fi ;;
-    *) argv+=("$1") ;; #eg: -logFile
+    *) argv+=("$1") ;; #eg: -logFile "$XDG_DATA_HOME/Shadowrun/returns-output.log"
   esac
   shift
 done
